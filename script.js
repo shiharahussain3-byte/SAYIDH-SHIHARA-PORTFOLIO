cca5765c770547e0c8a0e1d8a1fdc4d5d4d34b24
shiharahussain3-byte <shiharahussain3@gmail.com>
2026-08-06 22:33:03 +0530

    Add files via upload
    Update portfolio

--- script.js
+++ script.js
@@ -60,3 +60,40 @@ AOS.init({
     once:false,
     offset:120
 });
+
+const roles = [
+    "Machine Learning Enthusiast",
+    "Data Analyst",
+    "Web Developer",
+    "Python Programmer"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+    current = roles[i];
+
+    if (!isDeleting) {
+        document.getElementById("typing").textContent = current.substring(0, j++);
+    } else {
+        document.getElementById("typing").textContent = current.substring(0, j--);
+    }
+
+    if (!isDeleting && j === current.length + 1) {
+        isDeleting = true;
+        setTimeout(type, 1200);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % roles.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
